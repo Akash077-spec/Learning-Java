ac34e8f468f24488ae9b9a2c0a65196008b4770e
ACER <ak1406613@gmail.com>
2026-07-08 18:39:11 +0530

    Initial Java learning commit

---
+++ Sum.java
@@ -0,0 +1,13 @@
+public class Sum {
+
+    public static void main(String[] args) {
+
+        int num1 = 23;
+        int num2 = 25;
+        int num3 = 47;
+
+        int sum = num1 + num2 + num3;
+
+        System.out.println(sum);
+    }
+}
