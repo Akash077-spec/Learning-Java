c6b17c68cd546ae352856665838dce513c775fe5
ACER <ak1406613@gmail.com>
2026-07-10 20:41:11 +0530

    salary practice

---
+++ Basics/salary.java
@@ -0,0 +1,20 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class salary {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter employee name");
+        String name=sc.nextLine();
+        System.out.println("Enter monthly salary");
+        int num=sc.nextInt();
+        int yearlySalary;
+        yearlySalary= num*12;
+        System.out.println("Yearly Salary = "+yearlySalary );
+
+
+
+    }
+    
+}
