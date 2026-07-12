b6fc77c4dbf804f57bb4bce32ba2bfe5489349c4
ACER <ak1406613@gmail.com>
2026-07-12 18:26:07 +0530

    Arithmetic operator practice

---
+++ Basics/operator/ArithmeticOperator.java
@@ -0,0 +1,26 @@
+package Basics.operator;
+
+public class ArithmeticOperator {
+    public static void main(String[] args) {
+        System.out.println("===Arithmetic operator practice===");
+        int a = 65;
+        int b = 32;
+        int addition = a + b;
+        int subtraction = a - b;
+        int multiplication = a * b;
+        int division = a / b;
+        int modulus = a % b;
+        System.out.println(addition);
+        System.out.println(subtraction);
+        System.out.println(multiplication);
+        System.out.println(division);
+        System.out.println(modulus);
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(--a);
+        System.out.println(++a);
+        System.out.println(a--);
+        System.out.println(a);
+    }
+
+}
