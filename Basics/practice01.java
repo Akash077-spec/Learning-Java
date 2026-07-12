2e00fb4e466def6ab87dcd9d0572bb8ddad9f81f
ACER <ak1406613@gmail.com>
2026-07-12 08:42:00 +0530

    using hasNextInt

---
+++ Basics/practice01.java
@@ -0,0 +1,15 @@
+package Basics;
+import java.util.Scanner;
+
+public class practice01 {
+    public static void main(String [] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter number");
+        // int num= sc.nextInt();
+        // System.out.println(num);
+        // sc.nextLine();
+        boolean num= sc.hasNextInt();
+        System.out.println(num);
+    }
+    
+}
