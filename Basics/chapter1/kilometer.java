c881b28c997f3baf24db31c46cc2c6f3cee85b1e
ACER <ak1406613@gmail.com>
2026-07-12 14:06:02 +0530

    kilometer to miles java program

---
+++ Basics/chapter1/kilometer.java
@@ -0,0 +1,15 @@
+package Basics.chapter1;
+import java.util.Scanner;
+public class kilometer {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter distance in kilometers");
+        double distance = sc.nextDouble();
+        System.out.println( "Distance in kilometer is :" + distance);
+        double miles= distance * 0.621371d;
+        System.out.println("Total distance in miles is: " + miles);
+        sc.close();
+       
+    }
+    
+}
