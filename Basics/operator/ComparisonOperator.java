df17ce12cb68521fb632d340379366de1bdccb65
ACER <ak1406613@gmail.com>
2026-07-13 08:01:02 +0530

    Comparison operator

---
+++ Basics/operator/ComparisonOperator.java
@@ -0,0 +1,20 @@
+package Basics.operator;
+
+public class ComparisonOperator {
+    public static void main(String[] args) {
+        System.out.println("===This is a example practice for Comparison operator===");
+        int a = 10;
+        int b = 10;
+        float c = 25.0f;
+        System.out.println(a == b);
+        System.out.println(a == c);
+        System.out.println(b == c);
+        System.out.println(a >= b);
+        System.out.println(a >= c);
+        System.out.println(b >= c);
+        System.out.println(a <= b);
+        System.out.println(a <= c);
+        System.out.println(b <= c);
+
+    }
+}
