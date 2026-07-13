d5df88bc91b6fd0d56cccff81a02eb05c84b6f34
ACER <ak1406613@gmail.com>
2026-07-13 07:49:41 +0530

    Assignment Operator

---
+++ Basics/operator/AssignmentOperator.java
@@ -0,0 +1,14 @@
+package Basics.operator;
+
+public class AssignmentOperator {
+    public static void main(String[] args){
+        System.out.println("===Practice for assignment operator===");
+        int a = 8;
+        int b = 55;
+        a+= 99;// This mean a=a+99
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a);
+
+    }
+}
