90e5bfba8de5b28972661733e0acb30154adc1e6
ACER <ak1406613@gmail.com>
2026-07-13 09:25:43 +0530

    precedence of expression

---
+++ Basics/operator/expression.java
@@ -0,0 +1,15 @@
+package Basics.operator;
+
+public class expression {
+    public static void main(String[] args){
+        double a=77;
+        double b=66;
+        double x= 4;
+        double y=23;
+        double c=20;
+        System.out.println((x-y)/2);
+        System.out.println(((b*b)-4*a*c)/2*a);
+        System.out.println(a-b*x+y/c);
+
+    }
+}
