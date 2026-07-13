f365f5ae7b4993104743dd18e05ae3eb2603a701
ACER <ak1406613@gmail.com>
2026-07-13 11:24:45 +0530

    practice 02

---
+++ Basics/chapter2/practice03.java
@@ -0,0 +1,12 @@
+package Basics.chapter2;
+import java.util.Scanner;
+
+public class practice03 {
+    public static void main(String[] args){
+        int num=50;
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter number");
+        int num1=sc.nextInt();
+        System.out.println(num>num1);
+    }
+}
