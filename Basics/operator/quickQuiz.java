b8a84ae83cfbb58d374dae2120f72f20bf6fc1e4
ACER <ak1406613@gmail.com>
2026-07-13 10:00:15 +0530

    quiz 01

---
+++ Basics/operator/quickQuiz.java
@@ -0,0 +1,14 @@
+package Basics.operator;
+
+public class quickQuiz {
+    public static void main(String[] args){
+        int y =7;
+        int x=++y*8;
+        System.out.println(x);
+        char a ='z';
+        a++; 
+        
+        System.out.println(a);
+    }
+    
+}
