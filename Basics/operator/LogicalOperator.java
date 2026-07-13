c13d1dfbe43ad7f5c8dbcfa86388a477ca5ef77f
ACER <ak1406613@gmail.com>
2026-07-13 08:18:25 +0530

    Logical operator

--- Basics/operator/LogicalOperator.java
+++ Basics/operator/LogicalOperator.java
@@ -2,6 +2,18 @@ package Basics.operator;
 
 public class LogicalOperator {
     public static void main(String[] args){
-        
+        System.out.println("This is an example for Logical operator");
+        int a=66;
+        int b=56;
+        int c=18;
+        int k=66;
+        System.out.println(a==b && b>c);
+        System.out.println(a==b && b<c);
+        System.out.println(a!=b && b>c);
+        System.out.println(a==b || b>c);
+        System.out.println(a==k && b>c);
+        System.out.println(k>c || a<b);
+
+
     }    
 }
