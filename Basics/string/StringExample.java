6ce5d66ee911f2c0b01585084276c982b81717cd
ACER <ak1406613@gmail.com>
2026-07-15 16:48:03 +0530

    string methods

---
+++ Basics/string/StringExample.java
@@ -0,0 +1,27 @@
+package Basics.string;
+
+public class StringExample{
+    public static void main(String[] args){
+        String st = "My name is Akash kumar";
+        System.out.println(st);
+        // finding length ... use st.length()
+        System.out.println(st.length());
+        // change to lowercase
+        System.out.println(st.toLowerCase());
+        // uppercase
+        System.out.println(st.toUpperCase());
+        //using trim method we can eliminate unwanted spsces..
+        String name = "       Akash ";
+        System.out.println(name);
+        System.out.println(name.trim());// trim will remove all the unwanted spaces.
+        // substring(int start)
+       System.out.println(st.substring(3));
+       System.out.println(st.substring(3,9));
+       // replace charcater
+       System.out.println(name.replace('k','j'));
+       //name start with
+       System.out.println(st.startsWith("ak"));
+       System.out.println(st.indexOf(2));
+
+    }
+}
