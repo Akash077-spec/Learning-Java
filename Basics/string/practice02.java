f42478f5bbdb3a0f59ec7011a41ca1c34b5bc67d
ACER <ak1406613@gmail.com>
2026-07-16 17:02:27 +0530

    string practice03

---
+++ Basics/string/practice02.java
@@ -0,0 +1,19 @@
+package Basics.string;
+
+public class practice02 {
+    public static void main(String[] args) {
+        String firstname = "Akash";
+        String lastname = "Kumar";
+        System.out.println("Full Name : " + firstname + " " + lastname);
+        // The + operator joins two or more strings.
+        // This is called String Concatenation.
+
+        String a = "Java";
+        String b = "Programming";
+
+        System.out.println(a + b);
+        System.out.println(a + " " + b);
+        System.out.println("I love " + a);
+
+    }
+}
---
+++ Basics/string/practice03.java
@@ -0,0 +1,13 @@
+package Basics.string;
+import java.util.Scanner;
+public class practice03 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name : ");
+        String name = sc.nextLine();
+        System.out.println("Hello" +  " " + name);
+        System.out.println("Your name contain " + " "+  name.length()+ " characters.");
+
+    }
+    
+}
