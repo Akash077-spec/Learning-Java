0aa4fa97ba31b65e0b9c8af4aa598649be8069cb
ACER <ak1406613@gmail.com>
2026-07-16 18:45:33 +0530

    equal method in java

---
+++ Basics/string/equal.java
@@ -0,0 +1,12 @@
+package Basics.string;
+
+public class equal {
+    public static void main(String [] args){
+        String a = "Akash";
+        String b = " akash";
+        System.out.println(a.equals(b));
+
+
+    }
+    
+}
