36e547b729168e428535938f237fc306e2cf9368
ACER <ak1406613@gmail.com>
2026-07-16 18:19:12 +0530

    t method in java

---
+++ Basics/string/charAt.java
@@ -0,0 +1,13 @@
+package Basics.string;
+
+public class charAt {
+    public static void main(String [] args){
+        String program = " Java programming";
+        System.out.println(program.charAt(2));
+        System.out.println(" first character : " + program.charAt(0));
+         System.out.println(" middle  character : " + program.charAt(8));
+          System.out.println(" last character : " + program.charAt(program.length()-1));
+
+    }
+    
+}
---
+++ Basics/string/subString.java
@@ -0,0 +1,12 @@
+package Basics.string;
+
+public class subString {
+    public static void main(String[] args){
+        String name = "Akash kumar";
+        System.out.println(name.substring(0,6));
+        String college = "Grace college";
+        System.out.println(college.substring(6,13));
+    }
+
+    
+}
