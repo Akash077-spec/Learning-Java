4c60d3d0af58d1ab81058bbe6ac15d9478303949
ACER <ak1406613@gmail.com>
2026-07-18 17:16:11 +0530

    checking vote eligibility using if else

---
+++ Basics/ConditionalStatement/age.java
@@ -0,0 +1,18 @@
+package Basics.ConditionalStatement;
+
+import java.util.Scanner;
+
+public class age {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter age : ");
+        int age = sc.nextInt();
+        if(age>=18){
+            System.out.println("Eligible to vote ");
+        }else{
+            System.out.println("You are not eligible");
+        }
+
+    }
+
+}
