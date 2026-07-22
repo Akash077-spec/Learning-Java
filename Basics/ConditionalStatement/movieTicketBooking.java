35b8e34d98e4fa70d416a762ab09b47948d0ea57
ACER <ak1406613@gmail.com>
2026-07-22 11:30:48 +0530

    update movie ticket booking system

--- Basics/ConditionalStatement/movieTicketBooking.java
+++ Basics/ConditionalStatement/movieTicketBooking.java
@@ -1,31 +1,33 @@
 package Basics.ConditionalStatement;
+
 import java.util.Scanner;
+
 public class movieTicketBooking {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println( "========Movie Ticket Booking System========");
+        System.out.println("========Movie Ticket Booking System========");
         System.out.print("Enter you age : ");
         int age = sc.nextInt();
         sc.nextLine();
-        if(age>=18){
+        if (age >= 18) {
             System.out.print("Do you have a valid card?(yes/no) : ");
-            String id = sc.nextLine();
-            if(id.equalsIgnoreCase("yes") ){
+            String hasValidCrd = sc.nextLine();
+            if (hasValidCrd.equalsIgnoreCase("yes")) {
                 System.out.print("Enter the number of ticket : ");
-                int ticket= sc.nextInt();
-                if(ticket <=5){
+                int ticket = sc.nextInt();
+                if (ticket <= 5) {
                     System.out.println("Booking successfull ");
-                }else{
-                    System.out.println("You are not eligible to book ticket");
+                } else {
+                    System.out.println("Maximum 5 Tickets Allowed");
                 }
-            }else{
+            } else {
                 System.out.println("Valid card required ");
             }
 
-        }else{
+        } else {
             System.out.println("Not eligible");
         }
 
     }
-    
+
 }
