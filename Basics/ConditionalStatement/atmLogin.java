55181de5b2dd20e84440593faedbabdcd061506c
ACER <ak1406613@gmail.com>
2026-07-22 10:43:06 +0530

    atm pin exaple

--- Basics/ConditionalStatement/atmLogin.java
+++ Basics/ConditionalStatement/atmLogin.java
@@ -8,15 +8,18 @@ public class atmLogin {
         String pin = "1234";
         System.out.println("Enter pin : ");
         String pin2 = sc.nextLine();
-        System.out.println("Enter ammount");
-        double ammount = sc.nextDouble();
         if (pin.equals(pin2)) {
+             System.out.println("Enter ammount");
+        double ammount = sc.nextDouble();
             if (ammount <= 5000) {
                 System.out.println("Transcation successfull");
             } else {
                 System.out.println("Insufficient ammout ");
             }
+        }else{
+            System.out.println("invalid pin");
         }
+        sc.close();
     }
 
 }
