ade37fdac40a8d542e1f3bbba08f15ef67b1127a
ACER <ak1406613@gmail.com>
2026-07-29 18:44:47 +0530

    while  loop

---
+++ Basics/loop/whileLoop.java
@@ -0,0 +1,14 @@
+package Basics.loop;
+
+public class whileLoop {
+    public static void main(String [] args ){
+      int num = 1;
+
+while (num <= 20) {
+
+    if (num % 2 == 0) {
+        System.out.println(num);
+    }
+
+    num++;
+}}}
