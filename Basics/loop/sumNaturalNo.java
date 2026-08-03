c4bfe33656cba74072b28b058817b4353c7eef4b
ACER <ak1406613@gmail.com>
2026-08-03 11:03:37 +0530

    sum of natural number

---
+++ Basics/loop/sumNaturalNo.java
@@ -0,0 +1,18 @@
+package Basics.loop;
+
+import java.util.Scanner;
+
+public class sumNaturalNo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int num = sc.nextInt();
+        int sum = 0;
+        int i = 1;
+        while (i <= num) {
+            sum = i + sum;
+            i++;
+            System.out.println("sum = " + sum);
+        }
+    }
+}
