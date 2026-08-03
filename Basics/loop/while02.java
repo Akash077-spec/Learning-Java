aef0b748ec96e0995f7ffcfa04d629e4de1ed996
ACER <ak1406613@gmail.com>
2026-08-03 10:43:32 +0530

    while loop example practice

---
+++ Basics/loop/while02.java
@@ -0,0 +1,15 @@
+package Basics.loop;
+import java.util.Scanner;
+public class while02 {
+    public static void main(String[]args){
+             Scanner sc = new Scanner(System.in);
+             System.out.print("Enter the number : ");
+             int num = sc.nextInt();
+             int i = 1;
+             while(i<=num){
+                System.out.println(i);
+                i++;
+             }
+             sc.close();
+    }
+  }
