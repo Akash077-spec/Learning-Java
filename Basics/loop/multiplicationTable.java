95a38dfdf7e44fd5fb4206973246114dc0d19fec
ACER <ak1406613@gmail.com>
2026-08-03 12:17:07 +0530

    multiplication tavle using while loop

---
+++ Basics/loop/multiplicationTable.java
@@ -0,0 +1,16 @@
+package Basics.loop;
+import java.util.Scanner;
+public class multiplicationTable {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("======== Multiplication Table =======");
+        System.out.print("Enter number : ");
+        int num = sc.nextInt();
+        int i = 1;
+        while(i<=10){
+            System.out.println(num  +  " * "  +  i +  "="  +  (num*i));
+            i++;
+        }
+        sc.close();
+    }
+}
