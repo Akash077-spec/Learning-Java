d9c106a0b8468417abccd3b2cd1ed38a329c5adb
ACER <ak1406613@gmail.com>
2026-08-18 17:39:01 +0530

    hollow Square02 using for and if

---
+++ Basics/pattern/hollowSuare02.java
@@ -0,0 +1,14 @@
+package Basics.pattern;
+
+public class hollowSuare02 {
+    public static void main(String[] args) {
+        for(int row=1;row<=5;row++){
+            for(int col=1; col<=8;col++)
+            {
+                if(row==1||row==5||col==1||col==8){
+                    System.out.print("*");
+                }else{System.out.print(" ");}
+            }System.out.println();
+        }
+    }
+}
