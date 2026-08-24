0d69062f567e7ecdd3e3b0562d275ce37fbaf7fc
ACER <ak1406613@gmail.com>
2026-08-24 08:33:29 +0530

    revision question practice in variable

---
+++ Basics/RevisionQuestionPractice/RevisionQuestion1.java
@@ -0,0 +1,17 @@
+package Basics.RevisionQuestionPractice;
+
+public class RevisionQuestion1 {
+    public static void main(String[]args){
+        String name= "Akash";
+        int age= 22;
+        float CGPA= 8.5f;
+        char Grade='A';
+        boolean student=true;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(Grade);
+        System.out.println(CGPA);
+        System.out.println(student);
+
+    }
+}
---
+++ Basics/RevisionQuestionPractice/RevisionQuestion2.java
@@ -0,0 +1,5 @@
+package Basics.RevisionQuestionPractice;
+
+public class RevisionQuestion2 {
+    
+}
