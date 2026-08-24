2ad7cc606efabc728081525683d2494290fd5a64
ACER <ak1406613@gmail.com>
2026-08-24 16:47:00 +0530

    Practice Question 9B — continue

---
+++ Basics/RevisionQuestionPractice/PracticeQuestion9B.java
@@ -0,0 +1,11 @@
+package Basics.RevisionQuestionPractice;
+
+public class PracticeQuestion9B {
+    //Practice Question 9B — continue
+    public static void main(String[]args){
+    for(int i=1;i<=10;i++){
+        if(i==6){
+            continue;
+        }System.out.println(i);
+    }
+}}
