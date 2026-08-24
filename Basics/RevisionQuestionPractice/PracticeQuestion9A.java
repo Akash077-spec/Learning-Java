eb1b56fdf86390c3de5796cb23f86ec864dbfdf8
ACER <ak1406613@gmail.com>
2026-08-24 16:43:19 +0530

    Practice Question 9A — break

---
+++ Basics/RevisionQuestionPractice/PracticeQuestion9A.java
@@ -0,0 +1,13 @@
+package Basics.RevisionQuestionPractice;
+
+public class PracticeQuestion9A {
+    //Practice Question 9A — break
+    public static void main(String[]args){
+        for(int i=1;i<=10;i++){
+            if(i==6){
+                break;
+            }System.out.println(i);
+        }
+    }
+    
+}
