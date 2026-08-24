10889b2145f1a93972f23d91c3c2e1fcfa47e683
ACER <ak1406613@gmail.com>
2026-08-24 08:56:00 +0530

    REVISION ON  SCANNER

--- Basics/RevisionQuestionPractice/RevisionQuestion2.java
+++ Basics/RevisionQuestionPractice/RevisionQuestion2.java
@@ -1,5 +1,21 @@
 package Basics.RevisionQuestionPractice;
+import java.util.Scanner;
 
 public class RevisionQuestion2 {
+    public static void main(String [] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("======== STUDENTS DETAILS ========");
+        System.out.print("Name : ");
+        String name = sc.nextLine();
+        System.out.print("Age : ");
+        int age= sc.nextInt();
+        System.out.print("CGPA : ");
+        float CGPA= sc.nextFloat();
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(CGPA);
+        sc.close();
+
+    }
     
 }
