8e4e90118b743798e7c8133a773a2ff301f12f2e
ACER <ak1406613@gmail.com>
2026-08-25 16:34:16 +0530

    Pattern Revision 11B

---
+++ Basics/RevisionQuestionPractice/PatternRevision11B.java
@@ -0,0 +1,13 @@
+package Basics.RevisionQuestionPractice;
+
+public class PatternRevision11B {
+    public static void main(String[]args){
+        for(int row=5;row>=1;row--){
+            for(int col=1;col<=row;col++){
+                System.out.print(col);
+            }System.out.println();
+        }
+
+    }
+    
+}
