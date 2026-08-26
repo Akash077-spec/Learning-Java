8a44e207d6885e406483b9786dcba6efaf67fc4d
ACER <ak1406613@gmail.com>
2026-08-26 16:01:47 +0530

    feat: start Java arrays practice

---
+++ Array/array01.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class array01 {
+    public static void main(String[]args){
+        //Array creation 
+        int [] arr= {10,20,30,40,50};
+        // System.out.println(arr[1]);
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+    
+}
