c01f4863e29dd25044652d56605699e1e9a3ed80
ACER <ak1406613@gmail.com>
2026-08-26 16:07:57 +0530

    feat: add array traversal using for loop

---
+++ Array/array02.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class array02 {
+    public static void main(String[]args){
+        int arr[]={5,10,15,20,25,30};
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
