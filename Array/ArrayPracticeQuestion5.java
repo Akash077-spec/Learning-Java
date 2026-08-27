7fd38b28da4a9c52065ba7835600452b6cde2e13
Akash077-spec <ak1406613@gmail.com>
2026-08-27 16:43:53 +0530

    feat: find largest element in an array
    1. Create array
           ↓
    2. Take input
           ↓
    3. largest = arr[0]
           ↓
    4. Start comparing from index 1
           ↓
    5. arr[i] > largest ?
           ↓ YES
       largest = arr[i]
           ↓
    6. Print largest

---
+++ Array/ArrayPracticeQuestion5.java
@@ -0,0 +1,24 @@
+package Array;
+
+import java.util.Scanner;
+
+public class ArrayPracticeQuestion5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[5];
+
+        System.out.println("Enter number : ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+
+        }
+        int largest = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+            
+        }System.out.println("Largest element is :" + largest);
+
+    }
+}
